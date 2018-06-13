1868587dddeab77d6ccdeeb665afbcaf0b8bae4c
Christiaan Braad <c.braad@impulse-info.com>
2018-06-13 09:11:49 +0200

    step-by-step merge, when does the app fail

--- main/src/portal.js
+++ main/src/portal.js
@@ -1,16 +1,24 @@
 import 'zone.js'
 import * as singleSpa from 'single-spa'
 import { GlobalEventDistributor } from './globalEventDistributor'
-import { loadApp } from './helper'
+import { loadApp, loadModule } from './helper'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Navbar from './Navbar'
 
-async function init () {
+async function init (nav) {
   const globalEventDistributor = new GlobalEventDistributor()
 
-  await loadApp('agenda', '/agenda', '/module/agenda/singleSpaEntry.js', '/module/agenda/store.js', globalEventDistributor)
+  await loadModule('agenda', globalEventDistributor)
 
-  await loadApp('facturatie', '/facturatie', '/module/facturatie/singleSpaEntry.js', '/module/facturatie/store.js', globalEventDistributor)
+  await loadModule('facturatie', globalEventDistributor)
 
-  await loadApp('zorgplan', '/zorgplan', '/module/zorgplan/singleSpaEntry.js', '/module/zorgplan/store.js', globalEventDistributor)
+  await loadModule('zorgplan', globalEventDistributor)
+  // await loadApp('agenda', '/agenda', '/module/agenda/singleSpaEntry.js', '/module/agenda/store.js', globalEventDistributor)
+
+  // await loadApp('facturatie', '/facturatie', '/module/facturatie/singleSpaEntry.js', '/module/facturatie/store.js', globalEventDistributor)
+
+  // await loadApp('zorgplan', '/zorgplan', '/module/zorgplan/singleSpaEntry.js', '/module/zorgplan/store.js', globalEventDistributor)
 
   await loadApp('NOS', '/nos', '/module/url-loader/singleSpaEntry.js', null, null, {title: 'NOS', url: 'http://www.nos.nl'})
 
